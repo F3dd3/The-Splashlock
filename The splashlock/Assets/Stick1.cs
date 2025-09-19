b6f23c44bddaa0d46906b74740e20d02a43349a1
pooh23896 <97116980@st.deltion.nl>
2025-09-19 08:47:24 +0200

    wheel of frear animation

---
+++ The splashlock/Assets/Stick1.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Stick1 : MonoBehaviour
+{
+    [Header("Rotation Speed")]
+    public Vector3 rotationSpeed = new Vector3(0f, 50f, 0f);
+    // X, Y, Z graden per seconde
+
+    void Update()
+    {
+        // Draai object per frame
+        transform.Rotate(rotationSpeed * Time.deltaTime);
+    }
+}
